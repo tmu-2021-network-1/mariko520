e1fd3432cafbca3b9402a431f9a972547cfc4979
mariko520 <56290101+mariko520@users.noreply.github.com>
2021-05-08 13:25:07 +0900

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -13,7 +13,6 @@ const renderJson = (json) => {
    const studioTitleEn = document.createElement("span");
    studioTitleEn.className = 'studio-title-en';
    studioTitleEn.textContent = studio['name-en'];
-   studioTitle.textContent = studio['description-ja'];
    studioDiv.appendChild(studioTitle);
    studioDiv.appendChild(studioTitleEn);
    document.getElementById('studios').appendChild(studioDiv);
